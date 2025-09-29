b4528d637c36c4618dc35f4608f955783eed010b
onechunlin <865106110@qq.com>
2025-09-29 18:27:09 +0800

    feat: 页面增加 PWA

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
+import withPWA from "next-pwa";
 
 const nextConfig: NextConfig = {
   /* config options here */
 };
 
-export default nextConfig;
+export default withPWA({
+  dest: "public",
+  register: true,
+  skipWaiting: true,
+  disable: false, // 启用开发环境 PWA
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+})(nextConfig as any);
